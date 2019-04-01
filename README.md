95474fafb8e507f5de9a1220a3fdb5d1a40bf44d
KendaCodes <K.Lamandier@gmail.com>
2019-03-31 21:12:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+![alt!](public/entree.jpg)
+
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. run `npm run savage`
+2. Navigate to `localhost:3000`
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db, collection;
+
+const url = "mongodb+srv://film:film@movie-tye3w.mongodb.net/test?retryWrites=true";
+const dbName = "movies";
+
+app.listen(8000, () => {
+    MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`!");
+    });
+});
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  //console.log(db)
+  db.collection('movies').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {comments: result})
+  })
+})
+
+app.post('/comments', (req, res) => {
+  db.collection('movies').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/comments', (req, res) => {
+  db.collection('movies')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+app.put('/comments/downvote', (req, res) => {
+  db.collection('movies')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbDown:req.body.thumbDown + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/comments', (req, res) => {
+  db.collection('movies').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
